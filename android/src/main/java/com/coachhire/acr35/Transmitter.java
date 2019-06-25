9fbd433b88f220e5017e1f51e23b99402c5fcac6
C K <killcharles@gmail.com>
2019-06-25 16:49:04 +0100

    remove unused imports

--- android/src/main/java/com/coachhire/acr35/Transmitter.java
+++ android/src/main/java/com/coachhire/acr35/Transmitter.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.util.Log;
 
 import com.acs.audiojack.AesTrackData;
