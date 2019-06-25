ef0255d47b0bc868be06211b07689884cf057781
C K <killcharles@gmail.com>
2019-06-25 16:48:52 +0100

    try make ios build work

--- ios/RNAcr35.h
+++ ios/RNAcr35.h
@@ -1,4 +1,8 @@
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
 #import <React/RCTEventEmitter.h>
 
 #import <Foundation/Foundation.h>
